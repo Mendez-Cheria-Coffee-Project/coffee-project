eabbcd345f8c18d5e23014151fc5c63a915a0370
Mercy Osugo <cheryahmercy21@gmail.com>
2023-05-19 10:14:11 -0500

    'changed coffee names'

--- main.js
+++ main.js
@@ -43,20 +43,20 @@ function addCoffee(e) {
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 let coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
+    {id: 1, name: 'Jack Frost', roast: 'light'},
+    {id: 2, name: 'Frosty the Snowman', roast: 'light'},
+    {id: 3, name: 'Snowfall', roast: 'light'},
+    {id: 4, name: 'Black Ice', roast: 'medium'},
+    {id: 5, name: 'Oh Hail NO!', roast: 'medium'},
+    {id: 6, name: 'Blizzard Beans', roast: 'medium'},
+    {id: 7, name: 'Avalanche Affogato', roast: 'dark'},
+    {id: 8, name: 'Chillin Mocha Mint', roast: 'dark'},
+    {id: 9, name: 'Gusty Chai', roast: 'dark'},
+    {id: 10, name: 'Frostbite Frappe', roast: 'dark'},
+    {id: 11, name: 'Brewed Awakening', roast: 'dark'},
+    {id: 12, name: 'Sip Happens', roast: 'dark'},
+    {id: 13, name: 'Snowplace Like Roast', roast: 'dark'},
+    {id: 14, name: 'Cool Story Brew', roast: 'dark'},
 ];
 
 let tbody = document.querySelector('#coffees');
